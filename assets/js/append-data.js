b7944533928f2b27780b8e68a78a758b41b880a8
Duy Ho <hoduy2904@gmail.com>
2024-04-16 14:44:51 +0700

    final 1

---
+++ assets/js/append-data.js
@@ -0,0 +1,224 @@
+import banners from "../data/banner.json" assert { "type": "json" };
+import couples from "../data/couple-data.json" assert { "type": "json" };
+import stories from "../data/our-stories.json" assert { "type": "json" };
+import events from "../data/events.json" assert { "type": "json" };
+import subPeoples from "../data/subpeople.json" assert { "type": "json" };
+import albums from "../data/albums.json" assert { "type": "json" };
+
+var photoGalleries = [];
+if (albums) photoGalleries = albums;
+document.addEventListener("DOMContentLoaded", function (e) {
+  loadBanner();
+  loadCouple();
+  loadStories();
+  loadEvents();
+  loadSubPeople();
+  loadAlbums();
+  loadInitScript();
+});
+
+function loadBanner() {
+  let dataString = "";
+  for (let banner in banners) {
+    dataString += `<div class="img-item">
+      <img src="${banners[banner]}" alt="" />
+    </div>`;
+  }
+  $(".banner-carousel-image").html(dataString);
+}
+
+function loadCouple() {
+  let dataString = "";
+  for (let i in couples) {
+    dataString += `<div class="col-lg-6 col-sm-12">
+        <div
+          class="the-couple align-items-center align-items-md-start ${
+            i == 0 ? "reverse flex-column-reverse" : "flex-column"
+          } flex-md-row"
+        >
+          <div class="couple-img">
+            <img src="${couples[i].image}" alt="" />
+            <h4>${couples[i].position}</h4>
+          </div>
+          <div class="couple-about">
+            <div class="about-top text-center text-md-${i == 0 ? "right" : "left"}">
+              <h3>${couples[i].name}</h3>
+              <div class="cp-social">
+                <ul></ul>
+              </div>
+            </div>
+            <div class="cp-detail">
+              <div class="parent-infos text-center text-md-${i == 0 ? "right" : "left"}">
+                <div class="parent-info">
+                  <span class="parent-title">Con ông:</span>
+                  <b class="parent-name">${couples[i].father}</b>
+                </div>
+
+                <div class="parent-info">
+                  <span class="parent-title">Con bà:</span>
+                  <b class="parent-name">${couples[i].mother}</b>
+                </div>
+              </div>
+              <p class="text-center text-md-${i == 0 ? "right" : "left"}">
+                ${couples[i].description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  $(".row[data-postion=couple]").html(dataString);
+}
+
+function loadStories() {
+  let dataString = "";
+  for (let i in stories) {
+    dataString += `<div class="row ${i % 2 == 0 ? "reverse" : ""} story-item mt-5">
+        <div class="col-12 col-sm-6">
+          <div class="image-story">
+            <img src="${stories[i].image}" alt="" />
+          </div>
+        </div>
+        <div class="col-12 col-sm-6">
+          <div class="about-story">
+            <div class="expand-story ${i % 2 == 0 ? "" : "story-rt"}">
+              <div class="story-head">
+                <h3>${stories[i].title}</h3>
+                <h6>${stories[i].time}</h6>
+              </div>
+              <p>
+               ${stories[i].description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  $(".story-timeline").html(dataString);
+}
+
+function loadEvents() {
+  let dataString = "";
+  for (let i in events) {
+    dataString += `
+        <div class="event-stack">
+        <h3 class="text-center">${events[i].time}</h3>
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-5">
+            <div class="image-wrap">
+              <img
+                class="event-image"
+                src="${events[i].image}"
+                alt="${events[i].title}"
+              />
+            </div>
+          </div>
+          <div class="col-12 col-sm-12 col-md-7">
+            <div class="event-title text-center-sm">
+              <ul>
+                <li class="text-center">
+                  <i class="fa fa-calendar-check-o"></i> ${events[i].date}
+                </li>
+                <li class="text-center"><i class="fa fa-clock-o"></i> ${events[i].time}</li>
+              </ul>
+            </div>
+            <p class="text-center-sm">
+              <i class="fa fa-map-marker"></i>
+              ${events[i].address}
+            </p>
+            <a href="${events[i].mapLink}" class="text-center-sm" target="_blank">
+              Xem bản đồ
+              <i class="fa fa-caret-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  $("[data-position=events]").html(dataString);
+}
+
+function loadSubPeople() {
+  let dataString = "";
+  for (let i in subPeoples) {
+    dataString += `
+        <div id="${subPeoples[i].id}" class="tab-pane fade ${
+      i == 0 ? "active show" : ""
+    }">
+                <div class="row justify-content-center">
+                  <div class="col-12">
+                    <div class="our-team">
+                      <div class="team-img">
+                        <img
+                          src="${subPeoples[i].image}"
+                          alt=""
+                        />
+                        <div class="link">
+                          <ul></ul>
+                        </div>
+                      </div>
+                      <div class="team-content text-center">
+                        <h4 class="title">${subPeoples[i].name}</h4>
+                        <span class="post">${subPeoples[i].description}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>`;
+  }
+
+  $(".tab-content[data-position=impPeople]").html(dataString);
+}
+
+function loadAlbums() {
+  let dataString = "";
+  for (let i in albums) {
+    dataString += `
+        <div class="col-md-4 col-sm-6 col-xs-6 mix grid-item btn-see-more-gallery">
+              <div class="item">
+                <img
+                  src="${albums[i].src}"
+                  alt=""
+                />
+                <i class="fa fa-search"></i>
+              </div>
+            </div>`;
+  }
+
+  $(".masonry-gallery[data-position=albums]").html(dataString);
+}
+function loadInitScript() {
+  $(".banner-carousel-image").owlCarousel({
+    items: 1,
+  });
+  function masonryGridSetting() {
+    if ($(".masonry-gallery").length) {
+      var $grid = $(".masonry-gallery").masonry({
+        itemSelector: ".grid-item",
+        columnWidth: ".grid-item",
+        percentPosition: true,
+      });
+    }
+  }
+
+  masonryGridSetting();
+
+  // ALBUM GALLERIES
+  $(document).on("click", ".btn-see-more-gallery", function (e) {
+    e.preventDefault();
+    console.log(photoGalleries);
+    let indexNumber = $(this).data("index") || 0;
+    $(this).lightGallery({
+      thumbnail: true,
+      dynamic: true,
+      dynamicEl: photoGalleries,
+      download: true,
+      autoplay: true,
+      preload: 2,
+      appendSubHtmlTo: ".lg-item",
+      index: parseInt(indexNumber),
+    });
+  });
+}
---
+++ assets/js/main.js
@@ -0,0 +1,176 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+  if ($("#clock").length) {
+    function timeElapse(date) {
+      var current = Date();
+
+      var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+
+      var days = Math.floor(seconds / (3600 * 24));
+
+      if (days < 10) {
+        days = "0" + days;
+      }
+
+      seconds = seconds % (3600 * 24);
+
+      var hours = Math.floor(seconds / 3600);
+
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+
+      seconds = seconds % 3600;
+
+      var minutes = Math.floor(seconds / 60);
+
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+
+      seconds = seconds % 60;
+
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      var html =
+        '<div class="box"><div><div class="time">' +
+        days +
+        "</div> <span>" +
+        $("#clock").data("text-day") +
+        '</span> </div></div><div class="box"><div><div class="time">' +
+        hours +
+        "</div> <span>" +
+        $("#clock").data("text-hour") +
+        '</span> </div></div><div class="box"><div><div class="time">' +
+        minutes +
+        "</div> <span>" +
+        $("#clock").data("text-minute") +
+        '</span> </div></div><div class="box"><div><div class="time">' +
+        seconds +
+        "</div> <span>" +
+        $("#clock").data("text-second") +
+        "</span> </div></div>";
+
+      $("#clock").html(html);
+    }
+
+    var time = $("#clock").data("date");
+
+    $("#clock").countdown(time.replace(/-/g, "/"), function (event) {
+      if (event.type == "stoped") {
+        var together = new Date($("#clock").data("date"));
+
+        together.setHours(0);
+
+        together.setMinutes(0);
+
+        together.setSeconds(0);
+
+        together.setMilliseconds(0);
+
+        setInterval(function () {
+          timeElapse(together);
+        }, 1000);
+      } else {
+        var $this = $(this).html(
+          event.strftime(
+            "" +
+              '<div class="box"><div><div class="time">%D</div> <span>' +
+              $("#clock").data("text-day") +
+              "</span> </div></div>" +
+              '<div class="box"><div><div class="time">%H</div> <span>' +
+              $("#clock").data("text-hour") +
+              "</span> </div></div>" +
+              '<div class="box"><div><div class="time">%M</div> <span>' +
+              $("#clock").data("text-minute") +
+              "</span> </div></div>" +
+              '<div class="box"><div><div class="time">%S</div> <span>' +
+              $("#clock").data("text-second") +
+              "</span> </div></div>"
+          )
+        );
+      }
+    });
+  }
+
+  $(".important-people li").on("click", function (e) {
+    $(".important-people li").removeClass("active");
+    $(this).addClass("active");
+    console.log(this);
+  });
+
+  $(window).scroll(function () {
+    var windscroll = $(window).scrollTop();
+    if (windscroll > 80) {
+      $("nav.navbar").addClass("scroll navbar-light");
+      $("nav.navbar").removeClass("navbar-dark");
+    } else {
+      $("nav.navbar").removeClass("scroll navbar-light");
+      $("nav.navbar").addClass("navbar-dark");
+    }
+    $(".navbar-nav .nav-item .nav-link").each(function (i) {
+      let idElement = $(this).attr("href");
+      if ($(idElement).length) {
+        var posTop = $(idElement).position().top,
+          h = $($(this).attr("href")).height();
+        if (posTop <= windscroll && posTop + h > windscroll) {
+          $(".navbar-nav .nav-item").removeClass("active");
+          $(this).addClass("active");
+        } else {
+          $(this).removeClass("active");
+        }
+      }
+    });
+  });
+
+  let joined = localStorage.getItem("isShowJoin");
+  if (!joined) {
+    setTimeout(function (e) {
+      $("#modelId").modal("show");
+    }, 2000);
+
+    $(".btn-modal-join").on("click", function (e) {
+      let numberJoin = $("#numberJoin").val();
+      let url =
+        "https://docs.google.com/forms/d/e/1FAIpQLScFAXKG8Oc7wQp-SlwRm8zyZMxtBH89ggOoNAvktZ_I1Dq3dw/formResponse?&submit=Submit?usp=pp_url&entry.320452822=" +
+        numberJoin;
+      $.ajax({
+        url: url,
+        dataType: "jsonp",
+        success: function (data) {},
+        complete: function (data) {
+          localStorage.setItem("isShowJoin", true);
+        },
+      });
+    });
+  }
+
+  $(".audio-play").on("click", function () {
+    var audio = document.getElementById("audioPay");
+    if (audio.paused) {
+      $(this)
+        .html(`<span><svg xmlns="http://www.w3.org/2000/svg" width="25" height="27" fill="#fff" class="bi bi-volume-up-fill" viewBox="0 0 16 16">
+      <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"></path>
+      <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"></path>
+      <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"></path>
+    </svg></span>`);
+      audio.play();
+    } else {
+      $(this).html(`<span
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="27"
+        fill="#fff"
+        class="bi bi-volume-mute-fill"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"
+        ></path></svg
+    ></span>`);
+      audio.pause();
+    }
+  });
+});
